f925f206d398b80e9d914783514b79370c846019
umikami <ggoku02@gmail.com>
2022-08-11 22:56:43 -0400

    added surface to display

--- app.py
+++ app.py
@@ -6,11 +6,19 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("First Python Game")
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100,200))
+primary_blue = pygame.Color("#1f75c4")
+test_surface.fill(primary_blue)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    
+    screen.blit(test_surface, (0,0))
 
     pygame.display.update()
+    clock.tick(60)
