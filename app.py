c1868988dc88b54802e883e135cfc7070b6603e9
umikami <ggoku02@gmail.com>
2022-08-13 11:54:36 -0400

    added rectangles to surfaces

--- app.py
+++ app.py
@@ -10,14 +10,17 @@ pygame.display.set_caption("First Python Game")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('./font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('./graphics/Sky.png')
-ground_surface = pygame.image.load('./graphics/ground.png')
+sky_surface = pygame.image.load('./graphics/Sky.png').convert_alpha()
+ground_surface = pygame.image.load('./graphics/ground.png').convert_alpha()
 text_surface = test_font.render(
     'Hello World!', True, pygame.color.Color('#2a0878'))
 
-snail_surface = pygame.image.load("./graphics/snail/snail1.png")
-snail_x_position = 600
+snail_surface = pygame.image.load("./graphics/snail/snail1.png").convert_alpha()
+snail_rectangle = snail_surface.get_rect(midbottom = (810, 300))
 snail_speed = randint(2, 5)
+
+player_surface = pygame.image.load("./graphics/Player/player_walk_1.png").convert_alpha()
+player_rectangle = player_surface.get_rect(midbottom = (50,300))
 
 while True:
     for event in pygame.event.get():
@@ -28,11 +31,13 @@ while True:
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
     screen.blit(text_surface, (300, 50))
-    snail_x_position -= snail_speed
-    screen.blit(snail_surface, (snail_x_position,265))
-    
-    if snail_x_position < -50:
-       snail_x_position = 810
+    snail_rectangle.x -= snail_speed
+    screen.blit(snail_surface, snail_rectangle)
+    screen.blit(player_surface, player_rectangle)
+
+    # Reset postion and assign random speed
+    if snail_rectangle.x < -50:
+       snail_rectangle.x = 810
        snail_speed = randint(2, 5)
      
     pygame.display.update()
