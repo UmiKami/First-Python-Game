882f1d78af7b69794716a503537a6fb0eadf4889
umikami <ggoku02@gmail.com>
2022-08-19 21:45:51 -0400

    added time based spawner 50%

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import math
 from random import randint
 import pygame
 from sys import exit
@@ -20,9 +19,12 @@ game_font = pygame.font.Font('./font/Pixeltype.ttf', 50)
 sky_surf = pygame.image.load('./graphics/Sky.png').convert_alpha()
 ground_surf = pygame.image.load('./graphics/ground.png').convert_alpha()
 
+# Obstacle
 snail_surf = pygame.image.load("./graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom=(810, 300))
 snail_speed = randint(6, 10)
+
+obstacle_rect_list  = []
 
 player_surf = pygame.image.load(
     "./graphics/Player/player_walk_1.png").convert_alpha()
@@ -40,6 +42,10 @@ game_title_rect = game_title.get_rect(center = (400, 75))
 game_message = game_font.render('Press SPACE to start', True, '#00D8AE')
 game_message_rect = game_message.get_rect(center = (400, 350))
 
+# Timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 900)
+
 isGameOver = True
 startTime = 0
 score = 0
@@ -56,6 +62,8 @@ while True:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if player_rect.collidepoint(event.pos) and player_rect.bottom == 300:
                     player_gravity -= 22 + player_gravity
+            if event.type == obstacle_timer:
+                obstacle_rect_list.append(snail_surf.get_rect(bottomright = (randint(900,1100), 300)))
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE: 
                 isGameOver = False
@@ -67,12 +75,8 @@ while True:
         screen.blit(ground_surf, (0,300))
         score = display_score()
 
-        ### snail
-        snail_rect.x -= snail_speed
-        # Reset postion and assign random speed
-        if snail_rect.x < -50:
-            snail_rect.x = 810
-            snail_speed = randint(6, 20)
+        ### Obstacle
+        
 
         # on clossion
         if snail_rect.colliderect(player_rect):
